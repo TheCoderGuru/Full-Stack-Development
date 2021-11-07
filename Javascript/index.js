e19ab2d2eb3f3c4908b47b4593a08d849c1a7976
TheCoderGuru <shashreeshachindrasamuel14@gmail.com>
2021-11-07 12:48:37 -0400

    added some new code to the javascript folder

--- Javascript/index.js
+++ Javascript/index.js
@@ -817,3 +817,42 @@ const person = {
 const greeting = `Hello my name is ${person.name}! I am ${person.age} years old.`;
 
 console.log( greeting );
+
+
+const result = {
+  success: [ "max-length", "no-amd", "prefer-arrow-functions"],
+  failure: [ "no-var", "var-on-top", "linebreak" ],
+  skipped: [ "id-blacklist", "no-dup-keys" ]
+};
+
+function makeList( arr ){
+  const resultDisplayArray = [];
+  for( let i = 0; i < arr.length; i++ ){
+    resultDisplayArray.push( `<li class="text-warning">${arr[i]}`)
+  }
+
+  return resultDisplayArray;
+}
+
+const resultDisplayArray = makeList( result.failure );
+console.log( resultDisplayArray );
+
+
+// write concise object literal declarations using simple fields
+
+const createPerson = ( name, age, gender ) => ( { name, age, gender } )
+
+console.log( createPerson( "Zodiac Hasbro", 56, "male" ) );
+
+// write concise declarative functions
+
+const bicycle = {
+  gear: 2,
+  setGear( newGear ){
+    "use strict";
+    this.gear = newGear;
+  }
+};
+
+bicycle.setGear( 3 );
+console.log( bicycle.gear );
