97bc541dc1c624d9e740721d7e219794f4b13ed3
TheCoderGuru <shashreeshachindrasamuel14@gmail.com>
2021-11-14 21:15:04 -0400

    Added in some java code

--- Java/4_IntegerInput/IntegerInput.java
+++ Java/4_IntegerInput/IntegerInput.java
@@ -1,46 +1,83 @@
 import java.util.Scanner;
+
 public class IntegerInput{
 
   public static boolean validate( String[] args ){
+
     if( args.length != 4 ){
+      
       System.out.println( "Usage: <int> <int> <int> <int>" );
+      
       return false;
+    
     }
+    
     return true;
+  
   }
+  
   public static void version1( String[] args ){
+    
     Scanner system = new Scanner( System.in );
+    
     System.out.print( "Please enter four numbers: " );
+    
     int num1 = system.nextInt();
+    
     int num2 = system.nextInt();
+    
     int num3 = system.nextInt();
+    
     int num4 = system.nextInt();
 
     if( num1 == num2 && num2 == num3 && num3 == num4 ){
+      
       System.out.print( "The numbers are equal" );
+    
     }
+    
     else{
+      
       System.out.print( "Not all numbers are equal" );
+    
     }
+  
   }
 
   public static void version2( String[] args ){
+    
     int num1 = Integer.parseInt( args[ 0 ] );
+    
     int num2 = Integer.parseInt( args[ 1 ] );
+    
     int num3 = Integer.parseInt( args[ 2 ] );
+    
     int num4 = Integer.parseInt( args[ 3 ] );
 
     if( num1 == num2 && num2 == num3 && num3 == num4 ){
+      
       System.out.print( "The numbers are equal\n" );
+    
     }
+    
     else{
+      
       System.out.print( "Not all numbers are equal\n" );
+    
     }
+  
   }
+  
   public static void main( String[] args ){
+    
     if ( validate( args ) ) {
+      
       version2( args );
+      
       //version1( args );
+    
     }
+  
   }
+
 }
--- Java/Arithmetic_Operations/Arithmetic.java
+++ Java/Arithmetic_Operations/Arithmetic.java
@@ -1,58 +1,99 @@
 import java.util.Scanner;
+
 public class Arithmetic{
 
   public static int addition( int a , int b, String operator ){
+    
     int result = 0;
+    
     result = a + b;
+    
     return result;
+  
   }
 
   public static int subtraction( int a , int b, String operator ){
+    
     int result = 0;
+    
     result = a - b;
+    
     return result;
+  
   }
 
+
   public static int multiplication( int a , int b, String operator ){
+   
     int result = 0;
+    
     result = a * b;
+    
     return result;
+  
   }
 
   public static int division( int a , int b, String operator ){
+    
     int result = 0;
+    
     result = a / b;
+    
     return result;
+  
   }
 
   public static int modulation( int a , int b, String operator ){
+    
     int result = 0;
+    
     result = a % b;
+    
     return result;
   }
 
   public static void main( String[] args ) {
+    
     Scanner console = new Scanner( System.in );
+    
     System.out.println( "Please enter two numbers: " );
 
     int num1 = console.nextInt();
+    
     String operator = console.next();
+    
     int num2 = console.nextInt();
 
     if( operator.equals( "+" ) ){
+      
       System.out.printf( "%d + %d = %d", num1, num2, ( addition( num1, num2, operator ) ) );
+    
     }
+    
     else if( operator.equals( "-" ) ){
+      
       System.out.printf( "%d - %d = %d", num1, num2, ( subtraction( num1, num2, operator ) ) );
+    
     }
+    
     else if( operator.equals( "*" ) ){
+      
       System.out.printf( "%d * %d = %d", num1, num2, ( multiplication( num1, num2, operator ) ) );
+    
     }
+    
     else if( operator.equals( "/" ) ){
+      
       System.out.printf( "%d / %d = %d", num1, num2, ( division( num1, num2, operator ) ) );
+    
     }
+    
     else{
+      
       System.out.printf( "%d MOD %d = %d", num1, num2, ( modulation( num1, num2, operator ) ) );
+    
     }
+  
   }
+
 }
